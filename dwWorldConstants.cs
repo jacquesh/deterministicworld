e0cd90466a232934b74a095a914118805fd64d3b
Jacques Heunis <jaaksemail@gmail.com>
2013-06-29 16:19:19 +0200

    Add game frame specification constants

--- dwWorldConstants.cs
+++ dwWorldConstants.cs
@@ -6,11 +6,14 @@ namespace DeterministicWorld
     {
 
         public static string GAME_ID = "Warlocks";
-
         public static int GAME_VERSION = 1;
 
         public static int GAME_NET_PORT = 14242;
 
         public static int GAME_MAX_PLAYERS = 10;
+
+        public static int GAME_TICK_MILLIS = 50;
+
+        public static uint ORDER_DELAY_TICKS = 5;
     }
 }
