a0f543a9f0eab579ecf05745bedd009fd78980fc
Jacques Heunis <jaaksemail@gmail.com>
2013-07-05 22:11:30 +0200

    Add initial random class

---
+++ dwRandom.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DeterministicWorld
+{
+    public class dwRandom
+    {
+        private Random prng;
+
+        public dwRandom(int seed)
+        {
+            prng = new Random(seed);
+        }
+
+        public int get()
+        {
+            return prng.Next();
+        }
+
+    }
+}
