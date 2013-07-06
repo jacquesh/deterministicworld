efd0aaaca0484e0dbc7edd7ea2497c0fb939b6c9
Jacques Heunis <jaaksemail@gmail.com>
2013-07-06 13:58:30 +0200

    Make all world constants readonly

--- dwWorldConstants.cs
+++ dwWorldConstants.cs
@@ -4,15 +4,15 @@ namespace DeterministicWorld
     public abstract class dwWorldConstants
     {
 
-        public static string GAME_ID = "Warlocks";
-        public static int GAME_VERSION = 1;
+        public readonly static string GAME_ID = "Warlocks";
+        public readonly static int GAME_VERSION = 1;
 
-        public static int GAME_NET_PORT = 14242;
+        public readonly static int GAME_NET_PORT = 14242;
 
-        public static int GAME_MAX_PLAYERS = 10;
+        public readonly static int GAME_MAX_PLAYERS = 10;
 
-        public static int GAME_TICK_MILLIS = 50;
+        public readonly static int GAME_TICK_MILLIS = 50;
 
-        public static uint ORDER_DELAY_TICKS = 5;
+        public readonly static uint ORDER_DELAY_TICKS = 5;
     }
 }
